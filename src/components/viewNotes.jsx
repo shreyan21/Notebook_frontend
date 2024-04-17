1fe758de769adf83f3dbb985ff720b0d29b663ae
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-04-18 01:26:07 +0530

    Fifth commit

--- src/components/viewNotes.jsx
+++ src/components/viewNotes.jsx
@@ -4,7 +4,7 @@ import { loggedInContext } from "../context/LoginContext.js"
 
 const ViewNotes = (props) => {
     const [data, setData] = useState([])
-    const [error,setError]=useState('')
+    const [error, setError] = useState('')
     const navigate = useNavigate()
     const [filternotes, setFilternotes] = useState([])
     const { token } = useContext(loggedInContext)
@@ -21,18 +21,18 @@ const ViewNotes = (props) => {
     });
 
     const handleChange = (e) => {
-        setFormData({...formData,[e.target.name]:e.target.value})
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         // Handle form submission logic here
-        let temp=[]
+        let temp = []
         if (formData.title !== '' && formData.tag !== '') {
             for (const iterator of data) {
                 if ((formData.title === iterator.title) && (formData.tag === iterator.tag)) {
                     temp.push(iterator)
-                
+
                 }
             }
         }
@@ -65,47 +65,49 @@ const ViewNotes = (props) => {
             }
             setData(JSON.parse(localStorage.getItem('notes')).notes)
         }
-        catch(err){
-            setError(err)
-        }
-
-
-
-
-       
-
-    }
-
-
-
-
-    const fetchData = async () => {
-        try {
-            const res = await fetch('http://localhost:3001/notes/fetchNotes', { method: "GET", headers: { 'Content-Type': 'Application/json', 'Authorization': `${token}` } })
-            if (!res.ok) {
-                throw new Error('Error fetching data')
-            }
-            const result = await res.json()
-
-            let existingItem = JSON.parse(localStorage.getItem('notes')) || { notes: [] };
-
-            // Update existingItem with the fetched notes
-            existingItem.notes = result.notes;
-
-            // Update localStorage with the updated existingItem
-            localStorage.setItem('notes', JSON.stringify(existingItem));
-
-            // Set the data state with the fetched notes
-            setData(result.notes);
-
-        }
         catch (err) {
             setError(err)
         }
+
+
+
+
+
+
     }
 
 
-    useEffect(()=>{fetchData()},[data]);
+
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const res = await fetch('http://localhost:3001/notes/fetchNotes', { method: "GET", headers: { 'Content-Type': 'Application/json', 'Authorization': `${token}` } })
+                if (!res.ok) {
+                    throw new Error('Error fetching data')
+                }
+                const result = await res.json()
+
+                let existingItem = JSON.parse(localStorage.getItem('notes')) || { notes: [] };
+
+                // Update existingItem with the fetched notes
+                existingItem.notes = result.notes;
+
+                // Update localStorage with the updated existingItem
+                localStorage.setItem('notes', JSON.stringify(existingItem));
+
+                // Set the data state with the fetched notes
+                setData(result.notes);
+
+            }
+            catch (err) {
+                setError(err)
+            }
+        }
+        fetchData();
+    }, [data,token])
+
+
 
     return (
 
@@ -155,22 +157,47 @@ const ViewNotes = (props) => {
                                 </form>
                             </div>
                             <div className="col-lg-9 ">
-                               
-
-
-
-                                    <div className="container mt-5">
-                                        <div className="row">
 
 
 
 
-                                            {filternotes.length !== 0 ?
+                                <div className="container mt-5">
+                                    <div className="row">
 
-                                                (filternotes.map((val) => (
 
+
+
+                                        {filternotes.length !== 0 ?
+
+                                            (filternotes.map((val) => (
+
+                                                <div key={val._id} className=" col-sm-6 col-xs-12 col-lg-4 col-md-4 col-xl-3 mb-3">
+
+                                                    <div className="card h-100 shadow">
+                                                        <div className="card-header border-light">
+                                                            <Link to={`/${val._id}`} onClick={() => { props.setShow(true) }} className="card-link btn btn-sm float-end "  ><i className="fa-solid fa-pen"  ></i></Link>
+                                                            <h5 className="card-title">{val.title}</h5>
+                                                            <h6 className="card-subtitle mb-2 text-body-secondary">{val.tag}</h6>
+                                                        </div>
+                                                        <div className="card-body" >
+                                                            <p className="card-text">{val.description}</p>
+                                                        </div>
+                                                        <div className="card-footer border-light bg-white ">
+
+                                                            <Link to='/' onClick={() => handleDelete(val._id)} className="card-link btn btn-sm"><i className="fa-solid fa-trash"></i></Link>
+                                                            <div className="float-end ">{new Date(val.date).toDateString()}</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            )))
+
+                                            :
+
+
+                                            data.map((val) => (
+                                                <>
                                                     <div key={val._id} className=" col-sm-6 col-xs-12 col-lg-4 col-md-4 col-xl-3 mb-3">
-
                                                         <div className="card h-100 shadow">
                                                             <div className="card-header border-light">
                                                                 <Link to={`/${val._id}`} onClick={() => { props.setShow(true) }} className="card-link btn btn-sm float-end "  ><i className="fa-solid fa-pen"  ></i></Link>
@@ -187,47 +214,21 @@ const ViewNotes = (props) => {
                                                             </div>
                                                         </div>
                                                     </div>
+                                                </>
 
-                                                )))
-
-                                                :
-
-
-                                                data.map((val) => (
-                                                    <>
-                                                        <div key={val._id} className=" col-sm-6 col-xs-12 col-lg-4 col-md-4 col-xl-3 mb-3">
-                                                            <div className="card h-100 shadow">
-                                                                <div className="card-header border-light">
-                                                                    <Link to={`/${val._id}`} onClick={() => { props.setShow(true) }} className="card-link btn btn-sm float-end "  ><i className="fa-solid fa-pen"  ></i></Link>
-                                                                    <h5 className="card-title">{val.title}</h5>
-                                                                    <h6 className="card-subtitle mb-2 text-body-secondary">{val.tag}</h6>
-                                                                </div>
-                                                                <div className="card-body" >
-                                                                    <p className="card-text">{val.description}</p>
-                                                                </div>
-                                                                <div className="card-footer border-light bg-white ">
-
-                                                                    <Link to='/' onClick={() => handleDelete(val._id)} className="card-link btn btn-sm"><i className="fa-solid fa-trash"></i></Link>
-                                                                    <div className="float-end ">{new Date(val.date).toDateString()}</div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </>
-
-                                                )
+                                            )
 
 
-                                                )}
+                                            )}
 
-                                            <div className=" col-sm-6 col-xs-12 col-lg-4 col-xl-3 col-md-4 mb-3"   >
-                                                <div className="card bg-light shadow h-100" style={{ borderStyle: 'dashed' }}  >
+                                        <div className=" col-sm-6 col-xs-12 col-lg-4 col-xl-3 col-md-4 mb-3"   >
+                                            <div className="card bg-light shadow h-100" style={{ borderStyle: 'dashed' }}  >
 
-                                                    <div className="card-body  d-flex justify-content-center  align-items-center">
-                                                        <Link className=" text-decoration-none " onClick={addNote} to='/addNote' > <i className="fa-solid fa-square-plus fa-lg "></i>
-                                                        </Link>
-                                                    </div>
-
+                                                <div className="card-body  d-flex justify-content-center  align-items-center">
+                                                    <Link className=" text-decoration-none " onClick={addNote} to='/addNote' > <i className="fa-solid fa-square-plus fa-lg "></i>
+                                                    </Link>
                                                 </div>
+
                                             </div>
                                         </div>
                                     </div>
@@ -235,6 +236,7 @@ const ViewNotes = (props) => {
                             </div>
                         </div>
                     </div>
+                </div>
 
 
             )}
