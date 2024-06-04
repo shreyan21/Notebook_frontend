f8056221f11a7831429ff8b43d1e6e844b8d0c13
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-06-04 14:03:43 +0530

    Fixed bootstrap imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import Navbar from './components/Navbar';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
