7223bcdd3b01d154921f130903b3fae7f185e32e
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-07-01 12:07:08 +0530

    Improved UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-<<<<<<< HEAD
 
-import Navbar from './components/Navbar';
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
->>>>>>> 11e5fa5 (Imroved UI)
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import AddNote from './components/AddNote';
@@ -17,16 +13,6 @@ import Password from './components/Password.jsx';
 import LandingPage from './components/Landingpage.jsx';
 import UserProfile from './components/UserProfile.jsx';
 import { filterContext } from './context/FilteredContext.js';
-<<<<<<< HEAD
-=======
-import ResponsiveAppBar from './components/ResponsiveAppBar.jsx';
-import { imagecontext } from './context/ImageContext.js'
-// import ViewNotes from './components/viewNotes.jsx';
-import ViewNote from './components/ViewNote.jsx';
-import { Grow, Snackbar } from '@mui/material';
-import Footer from './components/Footer.jsx';
-
->>>>>>> 11e5fa5 (Imroved UI)
 function App() {
 
   const [show, setShow] = useState(false)
@@ -35,35 +21,10 @@ function App() {
     setOpen(false)
   }
   const [token, setToken] = useState(localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token')) : null)
-<<<<<<< HEAD
   const [note,setNote]=useState([])
-  const[filternote,setFilter]=useState([])
 
-  return (
-    <> 
-        <loggedInContext.Provider value={{ token, setToken }} >
-          <notecontext.Provider value={{note,setNote}}>
-            <filterContext.Provider value={{filternote,setFilter}}>
-          <Navbar setShow={setShow}  />
-
-          {!token && <LandingPage setShow={setShow} />}
-          {token && <ViewNotes show={show} setShow={setShow}  />}
-          <div className='container mt-2'>
-            <Routes>
-              <Route exact path='/:id' element={<EditNote show={show} setShow={setShow} />} />
-              <Route exact path='/password/:email' element={<Password show={show} setShow={setShow} />} />
-              <Route exact path='/userprofile' element={<UserProfile show={show} setShow={setShow} />} />
-              <Route exact path='/addnote' element={<AddNote show={show} setShow={setShow} />} />
-              <Route exact path='/signin' element={<SignIn show={show} setShow={setShow} />} />
-              <Route exact path='/signup' element={<SignUp show={show} setShow={setShow} />} />
-            </Routes>
-          </div>
-          </filterContext.Provider>
-          </notecontext.Provider>
-        </loggedInContext.Provider>
+ 
         
-=======
-  const [note, setNote] = useState([])
   const [filternote, setFilter] = useState([])
   const [img, setImg] = useState(localStorage.getItem('image')?JSON.parse(localStorage.getItem('image')):null)
   return (
@@ -94,7 +55,6 @@ function App() {
           </filterContext.Provider>
         </notecontext.Provider>
       </loggedInContext.Provider >
->>>>>>> 11e5fa5 (Imroved UI)
 
     </>
   );
