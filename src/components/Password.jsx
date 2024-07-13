eea5c1ca07bdfa54cd730a92046acc127cd0bf3d
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-07-13 23:44:40 +0530

    Fixed password problem

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import Modal from 'react-bootstrap/Modal'
 const Password = (props) => {
