c4f0201abbec30edfec40c480cfd7956167e099a
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-07-14 09:33:37 +0530

    fixed

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import {  useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import Modal from 'react-bootstrap/Modal'
 const Password = (props) => {
