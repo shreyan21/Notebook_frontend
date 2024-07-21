2cdb78296a781aeda723978695b2714bcb567649
Utkarsh Singh Rajawat <rajawatshrayansh@gmail.com>
2024-07-21 22:43:19 +0530

    Added some changes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import AddNote from './components/AddNote';
 import { Routes, Route } from 'react-router-dom';
-import { loggedInContext } from './context/LoginContext.js';
-import { notecontext } from './context/NoteContext.js';
-import EditNote from './components/EditNote';
+import { loggedInContext } from './context/LoginContext.jsx';
+import { notecontext } from './context/NoteContext.jsx';
+import EditNote from './components/EditNote.jsx';
 import { useState } from 'react';
 import Password from './components/Password.jsx';
 import LandingPage from './components/Landingpage.jsx';
@@ -17,7 +17,7 @@ import { Grow, Snackbar } from '@mui/material';
 import ResponsiveAppBar from './components/ResponsiveAppBar.jsx';
 import ViewNote from './components/ViewNote.jsx';
 import Footer from './components/Footer.jsx';
-import { profilecontext } from './context/ProfileContext.js';
+import { profilecontext } from './context/ProfileContext.jsx';
 function App() {
 
   const [show, setShow] = useState(false)
